60bc045449548d331d18f8be04125dcd79a0f30a
DOAN NGUYEN HA NAM <24521100@gm.uit.edu.vn>
2025-10-28 23:29:27 +0700

    Add files via upload

---
+++ Bài tập multiple socket connection/Ex3/Opening.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+namespace Ex3
+{
+    public partial class Opening : Form
+    {
+        public Opening()
+        {
+            InitializeComponent();
+        }
+        public const string ConnectionString =@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AuthDB;Integrated Security=True;";
+
+        public static string username = "ơ kìa tên đâu";
+
+        static public byte[] Sha256Bytes(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+            }
+        }
+        private void Opening_Load(object sender, EventArgs e)
+        {
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Hide();
+        }
+    }
+}
